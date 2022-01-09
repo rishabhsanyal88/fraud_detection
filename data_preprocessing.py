855800bc37b3e651a0733146aa2f8b15717df50d
rishabhsanyal88 <datasciencerishi@outlook.com>
2022-01-09 20:22:42 +0530

    Revert "added a new function"
    This reverts commit 7d4cb03814eb8b633863ec0705d8ad04662b4e3e.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,13 +1,1 @@
 print("adding all code related to fraud detection")
-
-#Creating simple sum function
-
-a = 9
-b = 8
-
-def sum(a,b):
-    c = a + b
-    return a
-
-print("added a new function which is not correct")
-
