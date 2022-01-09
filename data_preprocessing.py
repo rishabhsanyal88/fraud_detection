7d4cb03814eb8b633863ec0705d8ad04662b4e3e
rishabhsanyal88 <datasciencerishi@outlook.com>
2022-01-09 20:19:03 +0530

    added a new function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,13 @@
 print("adding all code related to fraud detection")
+
+#Creating simple sum function
+
+a = 9
+b = 8
+
+def sum(a,b):
+    c = a + b
+    return a
+
+print("added a new function which is not correct")
+
